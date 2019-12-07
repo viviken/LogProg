216268ccce416ff7d6bea0d6e0d27c761b80ea9d
viviken <bemve6@gmail.com>
2019-12-07 12:12:28 +0300

    /

--- tree.pl
+++ tree.pl
@@ -76942,3 +76942,32 @@ father("@I33584@", "@I33590@").
 mother("@I33585@", "@I33590@").
 father("@I33584@", "@I33591@").
 mother("@I33585@", "@I33591@").
+
+
+/** <examples>
+?- check("@I5579@","@I5580@").
+*/
+
+father1(Name, 0,Name).
+mother1(Name, 0,Name).
+
+father1(Name, Index, Name2):-
+    NewIndex is Index - 1,
+    father(Name3, Name),
+    father1(Name3, NewIndex ,Name2).
+
+mother1(Name, Index, Name2):-
+    NewIndex is Index - 1,
+    mother(Name3, Name),
+    mother1(Name3, NewIndex ,Name2).
+
+check(Name1, Name2):-
+    get_name(Name1_, Name1),
+    get_name(Name2_, Name2),
+    father1(Name1_, 3, F1),
+    father1(Name2_, 3, F2),
+    mother1(Name1_, 3, M1),
+    mother1(Name2_, 3, M2),
+    (F1 = F2;M1 = M2),
+    !
+.
